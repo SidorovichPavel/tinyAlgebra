d05320caa602bbe4af825d3d7ba00221752bb999
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-09-26 15:54:43 +0300

    vec normalize and matrix multiplication

--- include/tinyalgebralib/math/math.hpp
+++ include/tinyalgebralib/math/math.hpp
@@ -5,7 +5,18 @@
 
 namespace ta
 {
+	template <class T, size_t Dim>
+	Vector<T, Dim> normalize(const Vector<T, Dim> &vec)
+	{
+		auto rlen = 1.f / vec.length();
+		return vec * rlen;
+	}
+
 	template <class T, class U>
-	auto cross(const Vector<T, 3>& _V, const Vector<U, 3>& _U) noexcept ->
-		Vector<decltype(detail::declval_by_mul<T, U>()), 3>;
+	auto cross(const Vector<T, 3> &_V, const Vector<U, 3> &_U) noexcept -> Vector<detail::decltype_by_mul<T, U>, 3>;
+
+	mat4 operator*(const mat4 &A, const mat4 &B) noexcept;
+
+	vec4 operator*(const mat4 &mat, const vec4 &vec) noexcept;
+
 }
--- src/math/math.cpp
+++ src/math/math.cpp
@@ -2,16 +2,55 @@
 
 namespace ta
 {
+
 	template <class T, class U>
-	auto cross(const Vector<T, 3>& _V, const Vector<U, 3>& _U) noexcept ->
-		Vector<decltype(detail::declval_by_mul<T, U>()), 3>
+	auto cross(const Vector<T, 3> &_V, const Vector<U, 3> &_U) noexcept -> Vector<detail::decltype_by_mul<T, U>, 3>
 	{
 		using return_type = decltype(detail::declval_by_mul<T, U>());
 		return Vector<return_type, 3>{
-			_V.y()* _U.z() - _V.z() * _U.y(),
-				_V.z()* _U.x() - _V.x() * _U.z(),
-				_V.x()* _U.y() - _V.y() * _U.x()
-		};
+			_V.y() * _U.z() - _V.z() * _U.y(),
+			_V.z() * _U.x() - _V.x() * _U.z(),
+			_V.x() * _U.y() - _V.y() * _U.x()};
 	}
 
+	mat4 operator*(const mat4 &A, const mat4 &B) noexcept
+	{
+		mat4 result;
+
+		result[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0] + A[0][2] * B[2][0] + A[0][3] * B[3][0];
+		result[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1] + A[0][2] * B[2][1] + A[0][3] * B[3][1];
+		result[0][2] = A[0][0] * B[0][2] + A[0][1] * B[1][2] + A[0][2] * B[2][2] + A[0][3] * B[3][2];
+		result[0][3] = A[0][0] * B[0][3] + A[0][1] * B[1][3] + A[0][2] * B[2][3] + A[0][3] * B[3][3];
+
+		result[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0] + A[1][2] * B[2][0] + A[1][3] * B[3][0];
+		result[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1] + A[1][2] * B[2][1] + A[1][3] * B[3][1];
+		result[1][2] = A[1][0] * B[0][2] + A[1][1] * B[1][2] + A[1][2] * B[2][2] + A[1][3] * B[3][2];
+		result[1][3] = A[1][0] * B[0][3] + A[1][1] * B[1][3] + A[1][2] * B[2][3] + A[1][3] * B[3][3];
+
+		result[2][0] = A[2][0] * B[0][0] + A[2][1] * B[1][0] + A[2][2] * B[2][0] + A[2][3] * B[3][0];
+		result[2][1] = A[2][0] * B[0][1] + A[2][1] * B[1][1] + A[2][2] * B[2][1] + A[2][3] * B[3][1];
+		result[2][2] = A[2][0] * B[0][2] + A[2][1] * B[1][2] + A[2][2] * B[2][2] + A[2][3] * B[3][2];
+		result[2][3] = A[2][0] * B[0][3] + A[2][1] * B[1][3] + A[2][2] * B[2][3] + A[2][3] * B[3][3];
+		
+		result[3][0] = A[3][0] * B[0][0] + A[3][1] * B[1][0] + A[3][2] * B[2][0] + A[3][3] * B[3][0];
+		result[3][1] = A[3][0] * B[0][1] + A[3][1] * B[1][1] + A[3][2] * B[2][1] + A[3][3] * B[3][1];
+		result[3][2] = A[3][0] * B[0][2] + A[3][1] * B[1][2] + A[3][2] * B[2][2] + A[3][3] * B[3][2];
+		result[3][3] = A[3][0] * B[0][3] + A[3][1] * B[1][3] + A[3][2] * B[2][3] + A[3][3] * B[3][3];
+
+		return result;
+	}
+
+	vec4 operator*(const mat4 &mat, const vec4 &vec) noexcept
+	{
+		vec4 result;
+
+		result[0] = mat[0][0] * vec[0] + mat[0][1] * vec[1] + mat[0][2] * vec[2] + mat[0][3] * vec[3];
+		result[1] = mat[1][0] * vec[0] + mat[1][1] * vec[1] + mat[1][2] * vec[2] + mat[1][3] * vec[3];
+		result[2] = mat[2][0] * vec[0] + mat[2][1] * vec[1] + mat[2][2] * vec[2] + mat[2][3] * vec[3];
+		result[3] = mat[3][0] * vec[0] + mat[3][1] * vec[1] + mat[3][2] * vec[2] + mat[3][3] * vec[3];
+
+		return result;
+	}
+
+	
 }
