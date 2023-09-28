714bc42a5a1d0d68be20dc2f030818e5c2a952f8
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-09-28 06:03:14 +0300

    added no call body for mul type decl

--- include/tinyalgebralib/math/Utility.hpp
+++ include/tinyalgebralib/math/Utility.hpp
@@ -27,8 +27,11 @@ namespace ta::detail
 	bool is_same_pack_v = is_same_pack<T, Args...>::value;
 
 	template<class T, class U>
-	auto declval_by_mul() noexcept -> 
-		decltype(std::declval<T>() * std::declval<U>());
+	auto declval_by_mul() noexcept -> decltype(std::declval<T>() * std::declval<U>())
+	{
+		static_assert(std::__declval_protector<T>::__stop, "declval() must not be used!");
+      	return __declval<T>(0);
+	}
 
 	template<class T, class U>
 	using decltype_by_mul = decltype(declval_by_mul<T, U>());
