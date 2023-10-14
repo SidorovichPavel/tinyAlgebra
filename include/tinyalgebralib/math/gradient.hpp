8e3b57ab105a24177bbbc8477a9937c928b58361
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-10-15 01:21:21 +0300

    remove glm

--- include/tinyalgebralib/math/gradient.hpp
+++ include/tinyalgebralib/math/gradient.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
-#include "..\glm\glm.hpp"
 #include <type_traits>
 #include <functional>
+
+#include <tinyalgebralib/math/math.hpp>
 
 namespace ta
 {
 	template<class T, class SDF>
-	auto gradient(const glm::vec<3, T>& p, SDF& sdf, T delta = static_cast<T>(1) + std::numeric_limits<T>::epsilon())
-		-> std::enable_if_t<std::is_arithmetic_v<T> && std::is_same_v<decltype(sdf(p)), T>, glm::vec<3, T>>
+	auto gradient(const ta::Vector<T, 3>& p, SDF& sdf, T delta = static_cast<T>(1) + std::numeric_limits<T>::epsilon())
+		-> std::enable_if_t<std::is_arithmetic_v<T> && std::is_same_v<decltype(sdf(p)), T>, ta::Vector<T, 3>>
 	{
-		using vec3 = glm::vec<3, T>;
+		using vec3 = ta::Vector<T, 3>;
 		auto zero = static_cast<T>(0);
 		vec3 dx(delta, zero, zero);
 		vec3 dy(zero, delta, zero);
