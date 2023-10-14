d98325bca04fb0c82bf0923650e826455301f584
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-10-15 01:15:20 +0300

    change param name

--- include/tinyalgebralib/math/gradient.hpp
+++ include/tinyalgebralib/math/gradient.hpp
@@ -7,8 +7,8 @@
 namespace ta
 {
 	template<class T, class SDF>
-	auto gradient(const glm::vec<3, T>& p, SDF& _SDF, T delta = static_cast<T>(1) + std::numeric_limits<T>::epsilon())
-		-> std::enable_if_t<std::is_arithmetic_v<T> && std::is_same_v<decltype(_SDF(p)), T>, glm::vec<3, T>>
+	auto gradient(const glm::vec<3, T>& p, SDF& sdf, T delta = static_cast<T>(1) + std::numeric_limits<T>::epsilon())
+		-> std::enable_if_t<std::is_arithmetic_v<T> && std::is_same_v<decltype(sdf(p)), T>, glm::vec<3, T>>
 	{
 		using vec3 = glm::vec<3, T>;
 		auto zero = static_cast<T>(0);
@@ -16,10 +16,10 @@ namespace ta
 		vec3 dy(zero, delta, zero);
 		vec3 dz(zero, zero, delta);
 
-		vec3 grad = vec3(_SDF(p)) - vec3(
-			_SDF(p - dx),
-			_SDF(p - dy),
-			_SDF(p - dz));
+		vec3 grad = vec3(sdf(p)) - vec3(
+			sdf(p - dx),
+			sdf(p - dy),
+			sdf(p - dz));
 
 		return grad;
 	}
